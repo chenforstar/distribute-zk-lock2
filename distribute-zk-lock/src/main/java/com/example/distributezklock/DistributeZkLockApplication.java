77e7fd2dfadfbbacf7d3f585e634ee7ae47e6f2a
chenforstar <358496925@qq.com>
2021-09-27 18:23:15 +0800

    first commit

---
+++ distribute-zk-lock/src/main/java/com/example/distributezklock/DistributeZkLockApplication.java
@@ -0,0 +1,13 @@
+package com.example.distributezklock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributeZkLockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributeZkLockApplication.class, args);
+    }
+
+}
---
+++ distribute-zk-lock/src/test/java/com/example/distributezklock/DistributeZkLockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.distributezklock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributeZkLockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
